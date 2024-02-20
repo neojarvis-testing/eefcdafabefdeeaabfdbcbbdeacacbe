8d89fbe592973a96ac7015ebcafeb88f6702f764
neojarvis <techadmin@iamneo.ai>
2024-02-20 10:19:44 +0000

    initial-commit

---
+++ dotnetapp/Controllers/MovieController.cs
@@ -0,0 +1,6 @@
+﻿namespace dotnetapp.Controllers
+{
+    // Write your MovieController here...
+    // 1. Get all movies - GetMovies()
+    // 2. Add a movie - AddMovie(Movie movie)
+}
---
+++ dotnetapp/Models/ApplicationDbContext.cs
@@ -0,0 +1,7 @@
+﻿namespace dotnetapp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Write your ApplicationDbContext class here...
+    }
+}
